bbe09ba46c715e88c28a7d98a938d4bce162f8d5
tmichaely <tamirmic@outlook.com>
2025-03-17 18:04:08 -0700

    new react home page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="container">
+      <header>
+        <h1>CSS 475 Final Project</h1>
+        <h2>Team: Team 1</h2>
+        <h3>Members: Julia Nguyen, Jason Xu, Joel Yim, Tamir Michaely</h3>
       </header>
+
+      <div className="card">
+        <div className="button-group">
+          <button className="task-button">Tamir1: List API (40 points)</button>
+          <button className="task-button">Tamir2: Complex Query (60 points)</button>
+        </div>
+      </div>
     </div>
   );
 }
