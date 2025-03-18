4f5f60589ba39ccb09a5118ab8b240f59f3a6f3e
tmichaely <tamirmic@outlook.com>
2025-03-17 19:41:49 -0700

    update with logging

---
+++ src/Database.js
@@ -0,0 +1,53 @@
+const pg = require('pg')
+
+// create a config to configure both pooling behavior
+// and client options
+// note: all config is optional and the environment variables
+// will be read if the config is not present
+var config = {
+    user: 'postgres', // env var: PGUSER
+    database: 'db03', // env var: PGDATABASE
+    password: 'Database0305!', // env var: PGPASSWORD
+    host: 'localhost', // Server hosting the postgres database
+    port: 5432, // env var: PGPORT
+    max: 10, // max number of clients in the pool
+    idleTimeoutMillis: 30000 // how long a client is allowed to remain idle before being closed
+}
+
+const pool = new pg.Pool(config)
+
+async function query (q) {
+    console.log('query() ' + q)
+
+    const client = await pool.connect()
+    console.log('connect()')
+
+    let res
+    try {
+        await client.query('BEGIN')
+        try {
+            res = await client.query(q)
+            await client.query('COMMIT')
+        } catch (err) {
+            await client.query('ROLLBACK')
+            throw err
+        }
+    } finally {
+        client.release()
+    }
+    return res.rows;
+}
+
+async function main (queryStr) {
+    try {
+        const { rows } = await query(queryStr);
+        return rows;
+    } catch (err) {
+        console.log('Database ' + err)
+        return null;
+    }
+}
+//main('SELECT * FROM user where user = \'123\'')
+//main('SELECT * FROM Book LIMIT 10')
+
+//module.exports = { main };
--- src/Server.js
+++ src/Server.js
@@ -1,5 +1,9 @@
+//node --watch-path=. Server.js 
+
 const express = require("express");
 const cors = require("cors");
+const { main } = require("./Database");
+
 
 const app = express();
 const port = 3000;
@@ -10,9 +14,16 @@ app.get("/", (req, res) => {
     res.send("CSS 475 Web Service");
 });
 
-app.get("/tamir1/", (req, res) => {
-    res.send("Executing 1st API!");
-});
+app.get("/tamir1/", async (req, res) => {
+    try {
+        console.log('Tamir 1 running...')
+        const queryStr = "SELECT * FROM Book LIMIT 10";
+        const result = await main(queryStr);
+        res.json(result);
+    }   catch (error) {
+        res.status(500).send("Error executing 1st API!");
+    }
+  });
 
 app.get("/tamir2/", (req, res) => {
     res.send("Executing 2nd API!");
